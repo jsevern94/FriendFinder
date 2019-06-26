ad43b100b3d1eff5d4554cf19bf8971fb502f83a
jsevern94 <severnj@spu.edu>
2019-06-26 02:28:36 -0700

    updated build:css and watch functionality to work without moving the css files, added some dummy data, display now shows in section 11, refresh will bring you back to the beginning form.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsArray = [
     {
         name: "Barney Stinson",
         age: 34,
-        photo: "image.jpg",
+        photo: "https://media.gq.com/photos/55828b3f1177d66d68d5287c/master/w_1600%2Cc_limit/blogs-the-feed-how-i-met-your-mother-barney-stinson.jpg",
         values: [
             "3",
             "3",
@@ -15,7 +15,76 @@ var friendsArray = [
             "3",
             "3"
         ]
+    },
+    {
+        name: "Tyler Durden",
+        age: 31,
+        photo: "https://i7.lisimg.com/6252217/411full.jpg",
+        values: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "Phoebe Buffay",
+        age: 29,
+        photo: "https://cdn-images-1.medium.com/max/1200/1*z8jEgbyxJHtHb4ri_hy4lA.jpeg",
+        values: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Batman",
+        age: 35,
+        photo: "https://www.thewrap.com/wp-content/uploads/2016/03/bruce-wayne-george-clooney.jpg",
+        values: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Megan Fox",
+        age: 24,
+        photo: "https://cdn3.movieweb.com/i/article/9hZNmltDzsYWzWacybI2XptGDmzBFJ/798:50/Jonah-Hex-Movie-Megan-Fox-Ashamed.jpg",
+        values: [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ]
     }
+
 ];
 
 module.exports = friendsArray;
