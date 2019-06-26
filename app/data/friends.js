60e6a2a52eba048a1a4922c8963cbf112ddd8f87
jsevern94 <severnj@spu.edu>
2019-06-26 03:10:14 -0700

    added more dummy data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 var friendsArray = [
     {
-        name: "Barney Stinson",
-        age: 34,
-        photo: "https://media.gq.com/photos/55828b3f1177d66d68d5287c/master/w_1600%2Cc_limit/blogs-the-feed-how-i-met-your-mother-barney-stinson.jpg",
-        values: [
+        "name": "Barney Stinson",
+        "age": "34",
+        "photo": "https://media.gq.com/photos/55828b3f1177d66d68d5287c/master/w_1600%2Cc_limit/blogs-the-feed-how-i-met-your-mother-barney-stinson.jpg",
+        "values": [
             "3",
             "3",
             "3",
@@ -17,10 +17,10 @@ var friendsArray = [
         ]
     },
     {
-        name: "Tyler Durden",
-        age: 31,
-        photo: "https://i7.lisimg.com/6252217/411full.jpg",
-        values: [
+        "name": "Tyler Durden",
+        "age": "31",
+        "photo": "https://i7.lisimg.com/6252217/411full.jpg",
+        "values": [
             "2",
             "2",
             "2",
@@ -34,10 +34,10 @@ var friendsArray = [
         ]
     },
     {
-        name: "Phoebe Buffay",
-        age: 29,
-        photo: "https://cdn-images-1.medium.com/max/1200/1*z8jEgbyxJHtHb4ri_hy4lA.jpeg",
-        values: [
+        "name": "Phoebe Buffay",
+        "age": "29",
+        "photo": "https://cdn-images-1.medium.com/max/1200/1*z8jEgbyxJHtHb4ri_hy4lA.jpeg",
+        "values": [
             "1",
             "1",
             "1",
@@ -51,10 +51,10 @@ var friendsArray = [
         ]
     },
     {
-        name: "Batman",
-        age: 35,
-        photo: "https://www.thewrap.com/wp-content/uploads/2016/03/bruce-wayne-george-clooney.jpg",
-        values: [
+        "name": "Batman",
+        "age": "35",
+        "photo": "https://www.thewrap.com/wp-content/uploads/2016/03/bruce-wayne-george-clooney.jpg",
+        "values": [
             "5",
             "5",
             "5",
@@ -68,10 +68,10 @@ var friendsArray = [
         ]
     },
     {
-        name: "Megan Fox",
-        age: 24,
-        photo: "https://cdn3.movieweb.com/i/article/9hZNmltDzsYWzWacybI2XptGDmzBFJ/798:50/Jonah-Hex-Movie-Megan-Fox-Ashamed.jpg",
-        values: [
+        "name": "Megan Fox",
+        "age": "24",
+        "photo": "https://cdn3.movieweb.com/i/article/9hZNmltDzsYWzWacybI2XptGDmzBFJ/798:50/Jonah-Hex-Movie-Megan-Fox-Ashamed.jpg",
+        "values": [
             "4",
             "4",
             "4",
@@ -83,8 +83,109 @@ var friendsArray = [
             "4",
             "4"
         ]
+    },
+    {
+        "name": "Freddy Krueger",
+        "age": "200",
+        "photo": "http://media.altpress.com/uploads/2018/10/freddy_krueger_nightmare_on_elm_street.jpeg",
+        "values": [
+            "1",
+            "2",
+            "4",
+            "1",
+            "1",
+            "3",
+            "3",
+            "1",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Jonah Severn",
+        "age": "25",
+        "photo": "https://scontent.fhhr1-1.fna.fbcdn.net/v/t1.0-9/50815316_10215864344294943_1001920895544983552_n.jpg?_nc_cat=110&_nc_oc=AQmyj38Q0FYqeL1nikXdEwBpvgwCDwtVi1LvRFlQyYYKRWGCu328DODZNDxeod9lOxpY_n4TTBQ_75082x3K7Qo0&_nc_ht=scontent.fhhr1-1.fna&oh=45a445c0c349751f47db561452bad78e&oe=5D82FE88",
+        "values": [
+            "4",
+            "4",
+            "5",
+            "4",
+            "5",
+            "5",
+            "3",
+            "3",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "James Bond",
+        "age": "35",
+        "photo": "https://media.gannett-cdn.com/29906170001/29906170001_5781756481001_5781757848001-vs.jpg?pubId=29906170001",
+        "values": [
+            "1",
+            "3",
+            "4",
+            "1",
+            "3",
+            "4",
+            "2",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "Whoopi Goldberg",
+        "age": "63",
+        "photo": "https://thehill.com/sites/default/files/styles/article_full/public/whoopi_goldberg.jpg?itok=hFQ8_7vf",
+        "values": [
+            "1",
+            "2",
+            "4",
+            "1",
+            "2",
+            "4",
+            "2",
+            "2",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Amy Winehouse",
+        "age": "27",
+        "photo": "https://image-ticketfly.imgix.net/00/03/18/17/99-og.jpg?w=960&h=540&fit=crop&crop=top",
+        "values": [
+            "1",
+            "2",
+            "4",
+            "1",
+            "1",
+            "3",
+            "3",
+            "1",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Thelma Harper",
+        "age": "52",
+        "photo": "https://i.ytimg.com/vi/6EKZCXeYUj8/maxresdefault.jpg",
+        "values": [
+            "1",
+            "1",
+            "2",
+            "1",
+            "1",
+            "1",
+            "1",
+            "4",
+            "1",
+            "1"
+        ]
     }
-
-];
+]
 
 module.exports = friendsArray;
